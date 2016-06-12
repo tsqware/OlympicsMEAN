0ff21c0a9ffd19c6f8661172443bd60628f32ce8
Terrence Tyson <tsqware@gmail.com>
2016-06-11 21:16:53 -0400

    add call to express endpoint

--- client/src/app.js
+++ client/src/app.js
@@ -1,5 +1,9 @@
 import angular from 'angular'
 angular.module('olympics', [])
-.controller('SportsController', function() {
-  this.sports = ["Weightlifting", "Cycling"];
+.controller('SportsController', function($http) {
+  //this.sports = ["Weightlifting", "Cycling"];
+  $http.get('/sports')
+    .then((response) => {
+      this.sports = response.data;
+    });
 })
